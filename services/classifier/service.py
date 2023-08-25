d6451d787d70cd20d85856165addcd033e941324
Max <zhuravlevmo1@gmail.com>
2023-08-25 11:39:05 +0300

    update

--- services/classifier/service.py
+++ services/classifier/service.py
@@ -1,7 +1,7 @@
 import pickle
 
 from sklearn.neighbors import KNeighborsClassifier
-from transformers import pipeline
+from sentence_transformers import SentenceTransformer
 
 from .interface import Category
 from .interface import IClassifier
@@ -14,16 +14,12 @@ class Classifier(IClassifier):
     def __init__(self):
         settings = ClassifierSettings()
 
-        self.model_feature_extractor = pipeline("feature-extraction", framework="pt", model=settings.feature_extractor)
+        self.model_feature_extractor = SentenceTransformer(settings.feature_extractor)
         with open(f'{BASE_PATH}/services/classifier/models/{settings.classifier}.pickle', 'rb') as f:
             self.model_classifier: KNeighborsClassifier = pickle.load(f)
 
     def classify_many(self, data: list[Text]) -> list[Category]:
-        features = [
-            embedding[0].numpy().mean(axis=0)
-            for embedding in self.model_feature_extractor(data, return_tensors="pt")
-        ]
-
+        features = self.model_feature_extractor.encode(data)
         predictions = self.model_classifier.predict(features)
         categories = [Category(prediction) for prediction in predictions]
 
